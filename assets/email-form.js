b4b940eb2cbda785450563eaa291a56e4ddc5740
warriorlabsinj <155243026+warriorlabsinj@users.noreply.github.com>
2023-12-30 22:46:49 +0000

    Add files via upload

---
+++ assets/email-form.js
@@ -0,0 +1,18 @@
+var gForm = document.getElementById("gform");
+gForm.addEventListener('submit', (e) => {
+  
+  // Timeout is needed for form to properly submit with animation
+  setTimeout(function(){
+   
+  var subscribeForm = document.getElementById("subscribeForm")
+ subscribeForm.setAttribute("style", "-webkit-animation: fadeOut 1s; animation: fadeOut 1s;  animation-fill-mode: forwards;");
+ gForm.setAttribute("style", "display:none;");  
+ 
+subscribeForm.innerHTML = `<h1>Your wallet address Has Added To Our List!</h1> `   
+  
+subscribeForm.setAttribute("style", "-webkit-animation: fadeIn 1s; animation: fadeIn 1s;  animation-fill-mode: forwards;");  
+
+},500);
+  
+  
+})
